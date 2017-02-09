b18ccd20c8dd87be6da642ed64763d35bf94284b
Feroz Ahmmed <froghramar@gmail.com>
2017-02-09 13:14:58 +0600

    introducing angularjs

---
+++ frogweb.init.js
@@ -0,0 +1,23 @@
+﻿var app = angular.module("frogweb", []);
+app.directive("frogwebLinks", function () {
+    return {
+        template: "<h1>Hello World</h1>" +
+                "<h2>How Are You</h2>" +
+                "<p>I'm Fine</p>" +
+                "<h1>Hello World</h1>" +
+                "<h2>How Are You</h2>" +
+                "<p>I'm Fine</p>" +
+                "<h1>Hello World</h1>" +
+                "<h2>How Are You</h2>" +
+                "<p>I'm Fine</p>" +
+                "<h1>Hello World</h1>" +
+                "<h2>How Are You</h2>" +
+                "<p>I'm Fine</p>" +
+                "<h1>Hello World</h1>" +
+                "<h2>How Are You</h2>" +
+                "<p>I'm Fine</p>" +
+                "<h1>Hello World</h1>" +
+                "<h2>How Are You</h2>" +
+                "<p>I'm Fine</p>"
+    };
+});
